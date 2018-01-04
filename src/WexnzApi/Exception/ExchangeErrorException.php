c7e915feb1d5320bbc5c33a0232d84a5ae8e10a6
Oleksii Druzhchenko <oleksii.druzhchenko@gmail.com>
2018-01-04 13:27:17 +0200

    ExchangeErrorException added

---
+++ src/WexnzApi/Exception/ExchangeErrorException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace madmis\WexnzApi\Exception;
+
+/**
+ * Class ClientErrorException
+ * @package madmis\WexnzApi\Exception
+ */
+class ExchangeErrorException extends \RuntimeException implements WexnzApiExceptionInterface
+{
+
+}
